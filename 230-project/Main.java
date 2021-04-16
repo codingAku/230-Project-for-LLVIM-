a58eab7a04c6312aa66fa0b602cfc0a614efe4ea
codingAku <ubuntu_aku@LAPTOP-VTKEUCJK.localdomain>
2021-04-16 17:08:38 +0300

    First Commit

--- 230-project/Main.java
+++ 230-project/Main.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-// Your First Program
+
 public class Main{
     public static void main(String[] args) {
         String ece = "4+(4*3+5)*7";
