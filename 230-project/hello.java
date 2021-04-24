7fbc0e06f554eac6c8f4683e73802504159103ee
Demet Yayla <dmtyayla@gmail.com>
2021-04-25 02:27:57 +0300

    pushhhh

---
+++ 230-project/hello.java
@@ -0,0 +1,9 @@
+public class hello{
+    public static boolean error = false;
+    public static void main(String[] args){
+        String s = "ab";
+        s = s.substring(0,2);
+        System.out.println(s);
+    }
+
+}
